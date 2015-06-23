acfce500594c17e3b3897eeab6f93cd693299a3f
ccshark <gustav.larss@gmail.com>
2015-06-23 13:25:22 +0200

    update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,5 +11,5 @@ angular.module('starter.controllers', [])
 // A simple controller that shows a tapped item's data
 .controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
   // "Pets" is a service returning mock data (services.js)
-  $scope.pet = PetService.get($stateParams.petId);
+ // $scope.pet = PetService.get($stateParams.petId);
 });
